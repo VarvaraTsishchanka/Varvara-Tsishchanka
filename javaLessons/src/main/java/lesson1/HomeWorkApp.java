123cfaab876b83e573d2c76b76b5590d33638733
Varvara Tsishchanka <barbara_1798@mail.ru>
2024-12-29 15:29:31 +0300

    Initial commit

--- javaLessons/src/main/java/lesson1/HomeWorkApp.java
+++ javaLessons/src/main/java/lesson1/HomeWorkApp.java
@@ -4,6 +4,7 @@ public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
+        printColor();
     }
 
     public static void printThreeWords() {
@@ -23,6 +24,17 @@ if (c >= 0) {
     System.out.println("Сумма положительная");
 } else if (c<0) {
             System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int value;
+        value = 105;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.printlm("Зеленый");
         }
     }
 }
