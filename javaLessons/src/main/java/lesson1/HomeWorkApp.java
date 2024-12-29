d3536746ccd49658efd6e3bb4273b04987005ab3
Varvara Tsishchanka <barbara_1798@mail.ru>
2024-12-29 11:05:16 +0300

    Initial commit

--- javaLessons/src/main/java/lesson1/HomeWorkApp.java
+++ javaLessons/src/main/java/lesson1/HomeWorkApp.java
@@ -2,7 +2,9 @@ package lesson1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("new changes1");
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
     }
 }
