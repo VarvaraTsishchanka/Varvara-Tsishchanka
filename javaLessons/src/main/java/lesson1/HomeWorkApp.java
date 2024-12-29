1888675d11f08a8f02823c343627dadaaacfb923
Varvara Tsishchanka <barbara_1798@mail.ru>
2024-12-29 11:14:34 +0300

    Initial commit

--- javaLessons/src/main/java/lesson1/HomeWorkApp.java
+++ javaLessons/src/main/java/lesson1/HomeWorkApp.java
@@ -1,7 +1,7 @@
 package lesson1;
 
 public class HomeWorkApp {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {printThreeWords()}
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
