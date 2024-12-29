550607d1401fbbdec80c4622a99299ecc18a2307
Varvara Tsishchanka <barbara_1798@mail.ru>
2024-12-29 15:31:58 +0300

    Initial commit

--- javaLessons/src/main/java/lesson1/HomeWorkApp.java
+++ javaLessons/src/main/java/lesson1/HomeWorkApp.java
@@ -28,7 +28,7 @@ if (c >= 0) {
     }
     public static void printColor() {
         int value;
-        value = 105;
+        value = -1;
         if (value <= 0) {
             System.out.println("Красный");
         } else if (value > 0 && value <= 100) {
