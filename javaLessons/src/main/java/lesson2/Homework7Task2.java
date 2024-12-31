74f78ff5ef9c111d7b41fa98fe9978efc5b1a9a6
Varvara Tsishchanka <barbara_1798@mail.ru>
2024-12-31 16:09:46 +0300

    Initial commit

---
+++ javaLessons/src/main/java/lesson2/Homework7Task2.java
@@ -0,0 +1,14 @@
+package lesson2;
+
+public class Homework7Task2 {
+    static boolean isSumCorrecta (int a, int b) {
+        int c = a + b;
+        a = 5;
+        b = 3;
+        if (c >= 10 && c <= 20) {
+            return true;
+            return false;
+        }
+    }
+
+
