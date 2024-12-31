12b15d40ecae2faaf1bf7ef7dac2c9f75a13ebf9
Varvara Tsishchanka <barbara_1798@mail.ru>
2024-12-31 12:30:03 +0300

    Initial commit

---
+++ javaLessons/src/main/java/lesson2/Homework7Task1.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Homework7Task1 {
+    public static void main(String[] args) {
+        boolean myBoolean = true;
+        boolean myBoolean2 = false;
+        int a = 7;
+        int b = 5;
+        boolean result = (a + b) >= 10 && (a + b) <= 20;
+        System.out.println(result);
+    }
+}
