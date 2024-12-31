28802d499352c9db458a0f89ffaed3764828fe47
Varvara Tsishchanka <barbara_1798@mail.ru>
2024-12-31 16:33:57 +0300

    Initial commit

--- javaLessons/src/main/java/lesson2/Homework7Task1.java
+++ javaLessons/src/main/java/lesson2/Homework7Task1.java
@@ -1,12 +1,14 @@
 package lesson2;
 
 public class Homework7Task1 {
-    public static void main(String[] args) {
-        boolean myBoolean = true;
-        boolean myBoolean2 = false;
-        int a = 7;
-        int b = 5;
-        boolean result = (a + b) >= 10 && (a + b) <= 20;
-        System.out.println(result);
+    public static boolean isSumCorrect(int a, int b) {
+        a = 5;
+        b = 3;
+        int c = a + b;
+        if (c >= 10 && c <= 20) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
