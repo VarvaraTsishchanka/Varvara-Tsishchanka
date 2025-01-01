d864e556055338cc7a06d1e082f4475a28decf29
Varvara Tsishchanka <barbara_1798@mail.ru>
2025-01-01 13:19:24 +0300

    Initial commit

---
+++ javaLessons/src/main/java/lesson2/HomeWork7Task3.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+public class HomeWork7Task3 {
+    public static void main(String[] args) {
+        System.out.println(isNegative(7));
+    }
+
+    public static boolean isNegative(int x) {
+        x = 7;
+        if (x < 0) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
