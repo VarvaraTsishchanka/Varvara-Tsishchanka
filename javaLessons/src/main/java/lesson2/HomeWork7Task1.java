39e2ce89e1aef3d5fc3cda6667fd1a4daadde026
Varvara Tsishchanka <barbara_1798@mail.ru>
2025-01-01 19:30:30 +0300

    Initial commit

--- javaLessons/src/main/java/lesson2/HomeWork7Task1.java
+++ javaLessons/src/main/java/lesson2/HomeWork7Task1.java
@@ -1,4 +1,16 @@
 package lesson2;
 
-public class HomeWorkTask1 {
+public class HomeWork7Task1 {
+    public static void main(String[] args) {
+        System.out.println(isSumCorrect(7, 6));
+    }
+    static boolean isSumCorrect(int a, int b) {
+        a = 7;
+        b = 6;
+        return (a+b) >= 10 && (a+b) <= 20;
+    }
 }
+
+
+
+
