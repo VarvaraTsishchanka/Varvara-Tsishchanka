d248ec5f8859ef83649edfb6c8d6176852853d50
Varvara Tsishchanka <barbara_1798@mail.ru>
2025-01-01 15:03:42 +0300

    Initial commit

---
+++ javaLessons/src/main/java/lesson2/HomeWork7Task4.java
@@ -0,0 +1,14 @@
+package lesson2;
+
+public class HomeWork7Task4 {
+    public static void main(String[] args) {
+        HomeWork7Task4 example = new HomeWork7Task4();
+        example.printWordNTimes ("Варвара", 3);
+    }
+    public static void printWordNTimes(String a, int x) {
+        x=3;
+       for (int i = 0; i < x; i++) {
+           System.out.println("Варвара");
+       }
+    }
+}
