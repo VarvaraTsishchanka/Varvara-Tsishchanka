c46062f4570a8e8917dac4aba3820673a7164f74
Varvara Tsishchanka <barbara_1798@mail.ru>
2025-01-13 19:54:21 +0300

    Initial commit

--- javaLessons/src/main/java/lesson4/PaymentMain.java
+++ javaLessons/src/main/java/lesson4/PaymentMain.java
@@ -4,9 +4,9 @@ public class PaymentMain {
     public static void main(String[] args) {
         Payment payment = new Payment();
 
-        payment.addProduct("product1", 5.99);
-        payment.addProduct("product2", 10.99);
-        payment.addProduct("product3", 15.99);
+        payment.addProduct("хлеб", 5.99);
+        payment.addProduct("чай", 10.99);
+        payment.addProduct("салат", 15.99);
 
         payment.showProducts();
     }
